10177f154d1edd114d518a464bae5411475b782a
sonagj <sonagj@vt.edu>
2024-02-23 03:10:48 -0500

    Add Home - Header & Footer

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -3,34 +3,57 @@ import '../assets/css/global.css'
 import '../assets/css/AppHeader.css';
 import { Link } from 'react-router-dom';
 function AppHeader(){
-return(
+return (
+        <header className="container">
+        <section className="bookstore-logo-container">
+            <Link to={"/"}>
+                <img
+                    src={require('../assets/images/site/bookstore-booklogo.png')}
+                    alt="Paperpulse Logo"
+                    width="auto"
+                    height="auto"
+                />
+            </Link>
+            <Link to={"/"}>
+                <img
+                    src={require('../assets/images/site/bookstore-logoname.png')}
+                    alt="PaperPulse"
+                    width="auto"
+                    height="auto"
+                />
+            </Link>
+        </section>
 
-  <header className="container">
-    <section className="bookstore-logo">
-      <Link to="/">
-        <img
-          src={require('../assets/images/site/bookstore-logo.png')}
-          alt="Another Bookstore Logo"
-          width="150px"
-          height="auto"
-        />
-      </Link>
-    </section>
-    <section className="title-and-search-bar">
-      <h1>
-        <Link to="/" className="text-logo"> Another Bookstore </Link>
-      </h1>
-      <form action="">
-        <input type="text" className="search-bar" /><br />
-        <input type="submit" className="button search-button" value="Search" />
-      </form>
-    </section>
-    <section className="header-dropdown-and-cart">
-      <button className="button">cart (0)</button>
-      <button className="button">login</button>
-      <HeaderDropdown />
-    </section>
-  </header>
+        <section className="header-menu">
+            <button className="menu-button on-highlight">Home</button>
+            <div className="header-dropdown">
+                <HeaderDropdown/>
+            </div>
+        </section>
+
+        <section className="search-bar-and-cart">
+            <div className="container-mid-align">
+                <form action="">
+                    <label>
+                        <input type="text" className="search-bar" placeholder="Search"/>
+                    </label>
+                </form>
+            </div>
+            <button className="cart-button">
+                <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M16 16.9568C14.89 16.9568 14 17.8468 14 18.9568C14 19.4872 14.2107 19.9959 14.5858 20.371C14.9609 20.7461 15.4696 20.9568 16 20.9568C16.5304 20.9568 17.0391 20.7461 17.4142 20.371C17.7893 19.9959 18 19.4872 18 18.9568C18 17.8468 17.1 16.9568 16 16.9568ZM0 0.956802V2.9568H2L5.6 10.5468L4.24 12.9968C4.09 13.2768 4 13.6068 4 13.9568C4 14.4872 4.21071 14.9959 4.58579 15.371C4.96086 15.7461 5.46957 15.9568 6 15.9568H18V13.9568H6.42C6.3537 13.9568 6.29011 13.9305 6.24322 13.8836C6.19634 13.8367 6.17 13.7731 6.17 13.7068C6.17 13.6568 6.18 13.6168 6.2 13.5868L7.1 11.9568H14.55C15.3 11.9568 15.96 11.5368 16.3 10.9268L19.88 4.4568C19.95 4.2968 20 4.1268 20 3.9568C20 3.69159 19.8946 3.43723 19.7071 3.2497C19.5196 3.06216 19.2652 2.9568 19 2.9568H4.21L3.27 0.956802M6 16.9568C4.89 16.9568 4 17.8468 4 18.9568C4 19.4872 4.21071 19.9959 4.58579 20.371C4.96086 20.7461 5.46957 20.9568 6 20.9568C6.53043 20.9568 7.03914 20.7461 7.41421 20.371C7.78929 19.9959 8 19.4872 8 18.9568C8 17.8468 7.1 16.9568 6 16.9568Z"
+                        fill="#1C0461"/>
+                </svg>
+            </button>
+            <div className="container-mid-align">
+                <button className="login-button">
+                    <span className="login-text">Sign In</span>
+                </button>
+            </div>
+        </section>
+
+    </header>
 )
 }
 export default AppHeader;
--- client/src/components/HeaderDropdown.tsx
+++ client/src/components/HeaderDropdown.tsx
@@ -8,17 +8,23 @@ function HeaderDropdown() {
   return (
 
       <div className="header-dropdown">
-        <button className="button categories-button">Categories</button>
-        <ul>
-         {categoryList.map((item) =>    <li>
-             <Link to ="/categories">
-                 {item.name}</Link></li>)}
+          <div className="menu-category-down-arrow">
+              <button className="menu-button">Categories</button>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 10L11.165 15L15.33 10H7Z" fill="#1C0461"/>
+              </svg>
+          </div>
+          <ul>
+              {categoryList.map((item) => <li>
+                  <Link to="/categories">
+                      {item.name}</Link></li>)}
 
-        </ul>
+          </ul>
 
-</div>
+      </div>
 
-)
+  )
 }
+
 export default HeaderDropdown
 
